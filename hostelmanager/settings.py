caddfede581073e6b38bb60f9c7796c180a16f61
RavicharanN <ravicharan.vsp@gmail.com>
2017-07-21 19:49:32 +0530

    Make allowed hosts universal

--- hostelmanager/settings.py
+++ hostelmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1zecqhgf)2e)b$v1&py_(q_2-liwak(u4w8)bqusel2md*=e-8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finder',
 ]
 
 MIDDLEWARE = [
