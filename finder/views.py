40d0df49c97dc51810631e4b8fe22b8a72fdc903
RavicharanN <ravicharan.vsp@gmail.com>
2017-07-22 12:59:43 +0530

    Update 1

--- finder/views.py
+++ finder/views.py
@@ -14,7 +14,7 @@ def index(request) :
 		if(form.is_valid()):
 			form.save()
 			message = 'Successfully Registered'
-			return render(request,'index.html',{'form': form,'message': message})
+			return render(request,'details.html',{'message': message})
 
 		else :
 			form_error = "(Registration Failed. Have you filled all the details?)"
